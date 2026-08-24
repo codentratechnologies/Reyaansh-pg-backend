0b38a7bfc8aa4ffdc5e114f61bd28391c4553fac
Romit754 <romit9011@gmail.com>
2026-08-24 22:07:30 +0530

    feat: implement authentication and dashboard API endpoints using JWT and Firebase integration

--- DashBoard/urls.py
+++ DashBoard/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from .views import (
-    SetupAdminView, LoginView, ProtectedDataView, RefreshTokenView, 
+    SetupAdminView, LoginView, LogoutView, ProtectedDataView, RefreshTokenView, 
     AdminDetailsView, DashboardKPIView, DashboardChartsView, DashboardTablesView, DashboardAlertsView
 )
 
 urlpatterns = [
     path('setup-admin/', SetupAdminView.as_view(), name='setup_admin'),
     path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('refresh/', RefreshTokenView.as_view(), name='refresh_token'),
     path('protected-data/', ProtectedDataView.as_view(), name='protected_data'),
     path('admin-details/', AdminDetailsView.as_view(), name='admin_details'),
